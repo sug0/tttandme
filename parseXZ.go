8d2097cb55ee9691ee29acd9b325f930331a50d8
sugoiuguu <sugoiuguu@tfwno.gf>
2019-01-30 14:30:12 +0000

    better doc

--- parseXZ.go
+++ parseXZ.go
@@ -1,7 +1,9 @@
 package tttandme
 
 // Parses a xz compressed file; a temporary file will be used,
-// created at the user's temporary file directory.
+// created at the user's temporary file directory. This routine
+// assumes the user has the program "xz" in their PATH environment
+// variable.
 func NewParserXZ(p GenomeParser) GenomeParser {
     return NewParserCmd(p, "xz", "-c", "-d", "{}")
 }
